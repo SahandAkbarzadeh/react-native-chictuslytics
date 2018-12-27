06f4089907675fbcc56bbf95338b7d1b32085d93
sahandevs <sahandevs@gmail.com>
2018-12-27 11:51:24 +0330

    removed exmample app

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
 import { NativeModules } from 'react-native';
+import React, { Component } from "react";
+
 
 const { RNReactNativeChictuslytics } = NativeModules;
 
-export default RNReactNativeChictuslytics;
+export default class ChictusLytics extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        {this.props.children}
+      </View>
+    )
+  }
+
+}
