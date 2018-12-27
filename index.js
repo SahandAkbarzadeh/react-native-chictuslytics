729afdd3c3ec9eb03d4a006c703b8a5603dd4f71
sahandevs <sahandevs@gmail.com>
2018-12-27 15:36:15 +0330

    nc

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import {NativeModules, View, Text, ActivityIndicator} from 'react-native';
 import React, {Component} from "react";
 import {setJSExceptionHandler, setNativeExceptionHandler} from 'react-native-exception-handler';
 import Server, {REPORT_TYPES} from "./server";
-import RNExitApp from 'react-native-exit-app';
 
 /*
 
@@ -25,18 +24,19 @@ export default class ChictusLytics extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      state: STATES.NORMAL
+      state: STATES.NORMAL,
+      working: false,
     };
     Server.setUrl(this.props.server);
     Server.setConfigs(this.props.configs);
     const handleExceptions = (err, isFatal) => {
-      this.setState({state: STATES.CRASH});
-      Server.add("Crash Report " + this.props.titleExtraInformation(), "\n" + this.props.extraInformation(), REPORT_TYPES.CRASH, (data)=> {
+      this.setState({state: STATES.CRASH, working: true});
+      Server.add("Crash Report " + this.props.titleExtraInformation(), "\n" + this.props.extraInformation(), REPORT_TYPES.CRASH, (data) => {
         if (data.reportId === "") {
-          RNExitApp.exitApp();
+          this.setState({working: false})
         } else {
           Server.add_text_attachment(data.reportId, JSON.stringify(err), REPORT_TYPES.CRASH, () => {
-            RNExitApp.exitApp();
+            this.setState({working: false})
           })
         }
       })
@@ -49,12 +49,23 @@ export default class ChictusLytics extends Component {
       <View style={{width: '100%', height: '100%'}}>
         {this.state.state === STATES.NORMAL && (this.props.children)}
         {this.state.state === STATES.CRASH && (
-          <View style={{width: '100%', height: '100%', justifyContent: 'center', alignContent: 'center'}}>
+          <View style={{
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignContent: 'center',
+            flexDirection: 'row'
+          }}>
             <Text style={{textAlign: 'center'}}>
-              {"مشکلی پیش آمده است!\n نرم افزار تا چند ثانیه دیگر بسته خواهد شد..."}
+              {"مشکلی پیش آمده است!"}
+            </Text>
+            <Text>
+              {this.state.working ? 'در حال ارسال خطا به سرور برای رفع مشکل...' : 'از اتفاق پیش آمده متاسفیم لطفا نرم افزار را مجددا باز کنید.'}
             </Text>
             <View style={{width: 10, height: 10}}/>
+            {this.state.working &&
             <ActivityIndicator/>
+            }
           </View>
         )}
       </View>
@@ -66,6 +77,10 @@ export default class ChictusLytics extends Component {
 ChictusLytics.defaultProps = {
   enableDevCrashReport: false,
   enableReleaseShakeToReport: false,
-  extraInformation: () => { return ""},
-  titleExtraInformation: () => { return ""},
+  extraInformation: () => {
+    return ""
+  },
+  titleExtraInformation: () => {
+    return ""
+  },
 };
